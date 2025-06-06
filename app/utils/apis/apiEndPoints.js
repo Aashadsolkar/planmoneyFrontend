edce5279a3749466737e92536476d7c1bba93124
PallavP-07 <pallav811@gmail.com>
2025-06-06 14:33:03 +0530

    code clean

---
+++ app/utils/apis/apiEndPoints.js
@@ -0,0 +1,8 @@
+const BASE_URL = "https://admin.planmoney.in/api";
+
+// export const Customer_API_ENDPOINTS = {
+//   LOGIN: `${BASE_URL}/nurse/login`,
+//   NURSE_PROFILE: `${BASE_URL}/nurse/profile`,
+//   PATIENT_LIST: `${BASE_URL}/nurse/patientlist`,
+//   PATIENT_DETAILS: (id: any) => `${BASE_URL}/nurse/patientlist/${id}`,
+// };
