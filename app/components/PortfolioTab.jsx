e68070d77c58eaed13b3002802fcb7d6d988daae
Aashadsolkar <155612736+Aashadsolkar@users.noreply.github.com>
2025-06-18 16:20:04 +0530

    17-06 feedback changes done

--- app/components/PortfolioTab.jsx
+++ app/components/PortfolioTab.jsx
@@ -539,6 +539,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+        paddingVertical: 8
     },
     dropdownText: {
         fontSize: 12,
